5cd27db43ea90f2586fd0b7b01db93823f3e3459
Shankar <shangan.23@gmail.com>
2020-11-23 18:57:18 +0530

    feat(base): Base setup

---
+++ springboot-api/src/main/java/io/shangan23/springboot/SpringbootApiApplication.java
@@ -0,0 +1,13 @@
+package io.shangan23.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApiApplication.class, args);
+	}
+
+}
---
+++ springboot-api/src/test/java/io/shangan23/springboot/SpringbootApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.shangan23.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
